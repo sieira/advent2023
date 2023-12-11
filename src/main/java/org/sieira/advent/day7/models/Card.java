0ef4ccc8f0da514e2b596f97ba352f68e860cbd5
sieira <luis.sieira@spacebar.fr>
2023-12-11 17:31:12 +0100

    Add day 7

---
+++ src/main/java/org/sieira/advent/day7/models/Card.java
@@ -0,0 +1,45 @@
+package org.sieira.advent.day7.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Card {
+    JK('*'),
+    N2('2'),
+    N3('3'),
+    N4('4'),
+    N5('5'),
+    N6('6'),
+    N7('7'),
+    N8('8'),
+    N9('9'),
+    T('T'),
+    J('J'),
+    Q('Q'),
+    K('K'),
+    A('A');
+
+    private final char figure;
+
+    Card(char figure) {
+        this.figure = figure;
+    }
+
+    private static final Map<Character, Card> figureToCard = new HashMap<>();
+
+    static {
+        for (Card card : Card.values()) {
+            figureToCard.put(card.getFigure(), card);
+        }
+    }
+
+
+    private char getFigure() {
+        return figure;
+
+    }
+
+    public static Card getByFigure(char figure) {
+        return figureToCard.get(figure);
+    }
+}
