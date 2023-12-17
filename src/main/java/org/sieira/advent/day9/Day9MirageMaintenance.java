4c6dd5685359e099445db972110aa733a027ecc1
Luis Sieira <sieira@users.noreply.github.com>
2023-12-17 15:12:05 +0100

    Add day9 (#8)

---
+++ src/main/java/org/sieira/advent/day9/Day9MirageMaintenance.java
@@ -0,0 +1,17 @@
+package org.sieira.advent.day9;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Day9MirageMaintenance extends AbstractDay9MirageMaintenance {
+    protected Long extrapolateNextValue(@NotNull Stack<List<Long>> historyStack, Long previousResult) {
+        if (historyStack.empty()) {
+            return previousResult;
+        }
+        var currentRow = historyStack.pop();
+        var currentResult = currentRow.get(currentRow.size() - 1) + previousResult;
+        return extrapolateNextValue(historyStack, currentResult);
+    }
+}
